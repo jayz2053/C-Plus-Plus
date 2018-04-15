3fa74c8367cd778739629347c339a6502e04609e
jayz2053 <jpj19@students.uwf.edu>
2018-04-15 18:02:44 -0500

    wrote functions for removing nodes at various positions

--- Linked List/Linkd.cpp
+++ Linked List/Linkd.cpp
@@ -3,6 +3,7 @@
 #include "Linkd.h"
 using namespace std;
 
+//empty constructor
 template <class T>
 LinkdList<T>::LinkdList(){
   size = 0;
@@ -10,24 +11,70 @@ LinkdList<T>::LinkdList(){
   tail = NULL;
 }
 
+//constructors for a single node
 template <class T>
 LinkdList<T>::LinkdList(T){
   size = 1;
   head = new node()
 }
 
+//method for removing nodes at the end of the list
 template <class T>
 void LinkdList<T>::pop_back(){
 
+  //check for empty list nothing to pop
+  if(size == 0)
+    return;
+
+  //otherwise get the node previous to the tail
+  //and shift the address of tail
+  node<T> temp = tail->previous;
+  temp->next = NULL;
+  delete tail;
+  tail = temp;
+  size--;
 }
 
+//method for removing node at the front of the list
 template <class T>
 void LinkdList<T>::pop_front(){
 
+  //check for empty list nothing to pop
+  if(size == 0)
+    return;
+
+  //other wise get the second node in the list
+  //and shift the address of head
+  node<T> temp = head->next;
+  temp->previous = NULL;
+  delete head;
+  head = temp;
+  size--;
+
 }
 
+//method for removing an item in the middle of the list
 template <class T>
-void LinkdList<T>::remove_at(){
+void LinkdList<T>::remove_at(int index){
+
+  //bounds check
+  if(index > size -1){
+    cout << "index out of bounds";
+    return;
+  }
+
+  node<T> temp = head;
+
+  //step through loop until we reach the index
+  for(int i = 0; i < index; i++ ){
+    temp = temp->next;
+  }
+
+  //update the links between the next and previous nodes
+  temp->previous->next = temp->next;
+  temp->next->previous = temp->previous;
+
+  delete temp;
 
 }
 
